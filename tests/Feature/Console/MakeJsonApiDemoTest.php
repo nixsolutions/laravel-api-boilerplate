9eac527a73980738d9e52e2e12549aa89799c480
San4o2008 <alexandrkuzmenko.ak@gmail.com>
2017-11-07 13:17:34 +0200

    fix tests, fix console commands

--- tests/Feature/Console/MakeJsonApiDemoTest.php
+++ tests/Feature/Console/MakeJsonApiDemoTest.php
@@ -18,50 +18,24 @@ class MakeJsonApiDemoTest extends TestCase
     public function testMakeDemoCreateRemove()
     {
         $structure = [
-
-               'Controllers/LikesController.stub',
-                'Controllers/SkillsController.stub',
-                'Controllers/TeamsController.stub',
                 'Controllers/UsersController.stub',
 
-                'Models/Like.stub',
                 'Models/Role.stub',
-                'Models/Skill.stub',
-                'Models/Team.stub',
 
-                'migrations/add_foreign_keys_to_likes_table.stub',
-                'migrations/add_foreign_keys_to_membership_table.stub',
-                'migrations/add_foreign_keys_to_skills_table.stub',
-                'migrations/add_foreign_keys_to_teams_table.stub',
-                'migrations/create_likes_table.stub',
-                'migrations/create_membership_table.stub',
-                'migrations/create_skills_table.stub',
-                'migrations/create_teams_table.stub',
-
-                'seeds/JsonApiSeeder.stub',
-                'seeds/TeamsTableSeeder.stub',
-                'seeds/TeamUsersTableSeeder.stub',
-
-                'JsonApi/Likes/Hydrator.php',
-                'JsonApi/Likes/Request.php',
-                'JsonApi/Likes/Schema.php',
-                'JsonApi/Likes/Search.php',
-                'JsonApi/Likes/Validators.php',
-                'JsonApi/Skills/Hydrator.php',
-                'JsonApi/Skills/Request.php',
-                'JsonApi/Skills/Schema.php',
-                'JsonApi/Skills/Search.php',
-                'JsonApi/Skills/Validators.php',
-                'JsonApi/Teams/Hydrator.php',
-                'JsonApi/Teams/Request.php',
-                'JsonApi/Teams/Schema.php',
-                'JsonApi/Teams/Search.php',
-                'JsonApi/Teams/Validators.php',
+                'JsonApi/Users/Adapter.php',
                 'JsonApi/Users/Hydrator.php',
-                'JsonApi/Users/Request.php',
                 'JsonApi/Users/Schema.php',
-                'JsonApi/Users/Search.php',
                 'JsonApi/Users/Validators.php',
+
+                'JsonApi/Roles/Adapter.php',
+                'JsonApi/Roles/Hydrator.php',
+                'JsonApi/Roles/Schema.php',
+                'JsonApi/Roles/Validators.php',
+
+                'JsonApi/Activations/Adapter.php',
+                'JsonApi/Activations/Hydrator.php',
+                'JsonApi/Activations/Schema.php',
+                'JsonApi/Activations/Validators.php',
         ];
 
         vfsStream::copyFromFileSystem(base_path('stubs'), vfsStream::setup('app'));
