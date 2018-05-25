67041de372810bb0bf397ad120f54db3f7073141
San4o2008 <alexandrkuzmenko.ak@gmail.com>
2018-05-25 20:26:36 +0300

    remove stubs and remove make:demo command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,8 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        Commands\MakeJsonApiDemo::class,
-        Commands\MakeJsonApiDemoRemove::class
+        //
     ];
 
     /**
